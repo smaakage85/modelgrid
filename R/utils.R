5ac709c2f69be66fd481aac139665360dad2fe3e
Thomas Lin Pedersen <thomasp85@gmail.com>
2018-06-15 10:46:12 +0200

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,6 @@
-#' Pipe operator
-#'
-#' @name %>%
-#' @rdname pipe
-#' @keywords internal
 #' @export
 #' @importFrom magrittr %>%
 #' @importFrom caret train
-#' @usage lhs \%>\% rhs
-NULL
+magrittr::`%>%`
 
 globalVariables(".")
